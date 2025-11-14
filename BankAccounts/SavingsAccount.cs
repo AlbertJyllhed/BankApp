a705b26a6f4ce2d174bac92b045263f3176dc9d8
VISORKILLEN <viktor.gustavsson@live.se>
2025-11-14 14:56:45 +0100

    started with savingaccount

--- BankAccounts/SavingsAccount.cs
+++ BankAccounts/SavingsAccount.cs
@@ -1,6 +1,17 @@
 ﻿namespace BankApp.BankAccounts
 {
-    internal class SavingsAccount
+    internal class SavingsAccount : BankAccount
     {
+        internal decimal Intrest { get; set; } = 0.0186m;
+
+        internal SavingsAccount(string name, string currency, decimal intrest) : base(name, currency)
+        {
+            Intrest = intrest;
+        }
+
+        internal void CreateSavingsAccount()
+        {
+
+        }
     }
 }
