29c9669fd05cceb01f6bd644cb67af8e5348c3b6
VISORKILLEN <viktor.gustavsson@live.se>
2025-11-14 10:51:25 +0100

    Made input class methods

---
+++ Input.cs
@@ -0,0 +1,30 @@
+﻿namespace BankApp
+{
+    internal class Input
+    {
+        //Method to get string input from user
+        internal string GetString()
+        {
+            string input = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid input try again!");
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        //Method to get decimal input from the user
+        internal decimal GetDecimal()
+        {
+            decimal input;
+            while(decimal.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Invalid input try again!");
+            }
+            return input;
+        }
+        
+    }
+}
