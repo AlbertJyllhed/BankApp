77a2f06980fbfcc9473d4a9d9a10853246852851
Albert <Albert@LAPTOP-S4EA0KB6>
2025-11-17 09:57:35 +0100

    Added Admin and updated Data class.

--- Users/Admin.cs
+++ Users/Admin.cs
@@ -1,8 +1,30 @@
 ﻿namespace BankApp.Users
 {
-    internal class Admin
+    internal class Admin : User
     {
+        internal Admin(string name, string password) : base(name, password)
+        {
 
+        }
 
+        internal void UpdateCurrency()
+        {
+            Console.WriteLine("Which currency do you want to update?");
+            var currency = Data.GetCurrency();
+            Console.Write($"Type the new exchange rate for {currency}: ");
+            Data.currency[currency] = Input.GetDecimal();
+        }
+
+        internal void CreateUser()
+        {
+            Console.WriteLine("Creating new user.");
+            Console.Write("Type username: ");
+            var name = Input.GetString();
+            Console.Write("Type password: ");
+            var password = Input.GetString();
+            var user = new User(name, password);
+            Data.users.Add(user);
+            Console.WriteLine($"User {name} created.");
+        }
     }
 }
