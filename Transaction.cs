35e3d12cb5f18d60fcaab0ad14303c16f191df2d
CajsaMartensson <cajsa.maartensson@gmail.com>
2025-11-24 14:43:33 +0100

    Added a transaction-class.

---
+++ Transaction.cs
@@ -0,0 +1,11 @@
+﻿using BankApp.BankAccounts;
+
+namespace BankApp
+{
+    internal class Transaction
+    {
+        public BankAccount From { get; set; }
+        public BankAccount To { get; set; }
+        public decimal TransactionAmount { get; set; }
+    }
+}
