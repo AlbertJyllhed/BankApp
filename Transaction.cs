87572a17fe571b9e08bfd2b1718b8df4ac6f3111
Sara Andersson <paphatsara1995@hotmail.com>
2025-11-28 10:56:41 +0100

    Added CreateTransaction method in BankAccount

--- Transaction.cs
+++ Transaction.cs
@@ -2,27 +2,21 @@
 {
     internal class Transaction
     {
-        //private List<Transaction> _transactions = [];
-        internal string FromUser { get; set; }
-        internal string ToUser { get; set; }
         internal decimal Amount { get; set; }
         internal DateTime TimeStamp { get; }
         internal string Description { get; set; }
 
 
-        internal Transaction(string fromUser, string toUser, decimal amount, string description)
+        internal Transaction(decimal amount, string description)
         {
-            FromUser = fromUser;
-            ToUser = toUser;
             Amount = amount;
             TimeStamp = DateTime.Now;
             Description = description;
         }
         public override string ToString()
         {
-            return $"{TimeStamp:G} - {Description} ({Amount})";
+            return $"{Description} {Amount} {TimeStamp:G} ";
         }
-
-
+       
     }
 }
